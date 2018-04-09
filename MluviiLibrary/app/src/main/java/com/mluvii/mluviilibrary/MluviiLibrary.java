7f9fa11bb1b1bdc0a8d81d77a6056c110fe1f6cf
fogatond <ondrej.fogatos@icord.cz>
2018-04-09 11:32:21 +0200

    Mluvii Library and Mluvii App for testing library

---
+++ MluviiLibrary/app/src/main/java/com/mluvii/mluviilibrary/MluviiLibrary.java
@@ -0,0 +1,272 @@
+package com.mluvii.mluviilibrary;
+
+import android.app.Activity;
+import android.net.http.SslError;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Looper;
+import android.util.Log;
+import android.webkit.JavascriptInterface;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.content.Intent;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.concurrent.Callable;
+import android.os.Handler;
+
+/**
+ * Created by jaros on 2/23/2018.
+ */
+
+public class MluviiLibrary {
+
+    /**
+     * Pri prvnim volani naplnime hodnotou chatu - kvuli handlovani kliku na odkaz v chatu
+     */
+    private static String CHAT_URL;
+    /**
+     * Interface na volani Android library z webview
+     */
+
+
+
+    private static class InterfaceBetweenJavascriptAndLibrary{
+
+        /**
+         * Nastaveni stavu operatora na balicku zobrazenem na strance nacetle ve webview
+         * @param value Operator status
+         */
+        @JavascriptInterface
+        public void setOperatorStatus(String value) {
+            Log.d("MLUVII_STATUS","Mluvii status changed to: "+value);
+            if(value.equals("1") && onlineFunc != null){
+                try {
+                    onlineFunc.call();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if(value.equals("0") && offlineFunc != null){
+                try {
+                    offlineFunc.call();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if(value.equals("2") && busyFunc != null){
+                try {
+                    busyFunc.call();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        /**
+         * Volani zavreni chatu z webview
+         */
+        @JavascriptInterface
+        public void closeChat(){
+            resetUrl();
+            if(closeChatFunc != null){
+                try {
+                    closeChatFunc.call();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private static WebView mluviiWebView = null;
+    private static Callable<Void> onlineFunc = null;
+    private static Callable<Void> offlineFunc = null;
+    private static Callable<Void> busyFunc = null;
+    private static Callable<Void> closeChatFunc = null;
+    private static Callable<Void> chatLoaded = null;
+
+    private static String injectedString = "var _close = window.close; window.close = function (){ if(window['mluviiLibrary']){ window['mluviiLibrary'].closeChat(); } _close();}";
+
+    /**
+     * Nastaveni funkce, ktera se vola po nacteni strankz s chatem / Stanka otevrena po zavolani funkce runCHAT
+     * @param function Function to call when chat page is loaded
+     */
+    public static void setChatLoadedCallback(Callable<Void> function){
+        chatLoaded = function;
+    }
+
+    /**
+     * Nastaveni funkce, ktera se vola pri zmene stavu na online
+     * @param function Function to call
+     */
+    public static void setStatusOnlineCallback(Callable<Void> function){
+        onlineFunc = function;
+    }
+
+    /**
+     * Nastaveni funkce, ktera se vola pri zmene stavu na Offline
+     * @param function Function to call
+     */
+    public static void setStatusOfflineCallback(Callable<Void> function){
+        offlineFunc = function;
+    }
+
+    /**
+     * Nastaveni funkce, ktera se vola pri zmene stavu na busy
+     * @param function Function to call
+     */
+    public static void setStatusBusyCallback(Callable<Void> function){
+        busyFunc = function;
+    }
+
+    /**
+     * Nastaveni funkce, ktera se vola pri zavreni chatu
+     * @param function Function to call
+     */
+    public static void setCloseChatFunc(Callable<Void> function){
+        closeChatFunc = function;
+    }
+
+    /**
+     * Spusteni chatu na zatim skyte webview
+     */
+    public static void runChat(){
+        if(Build.VERSION.SDK_INT  >= 19) {
+            Log.d("Cool evaluate");
+            mluviiWebView.evaluateJavascript(injectedString, null);
+        } else {
+            Log.d("Low evaluate");
+            mluviiWebView.loadUrl("javascript: openChat()");
+        }
+    }
+
+    private static String createUrlString(String url, String companyId, String tenantId, String presetName, String language){
+        StringBuilder builder = new StringBuilder();
+        builder.append("https://");
+        builder.append(url);
+        builder.append("/MobileSdkWidget");
+        builder.append("?c=");
+        builder.append(companyId);
+        if(tenantId != null) {
+            builder.append("&t=");
+            try {
+                builder.append(URLEncoder.encode(tenantId,"utf-8"));
+            } catch (UnsupportedEncodingException e) {
+                builder.append(tenantId);
+            }
+        }
+        if(language != null) {
+            builder.append("&l=");
+            try {
+                builder.append(URLEncoder.encode(language,"utf-8"));
+            } catch (UnsupportedEncodingException e) {
+                builder.append(language);
+            }
+        }
+        if(presetName != null){
+            builder.append("&p=");
+            try {
+                builder.append(URLEncoder.encode(presetName,"utf-8"));
+            } catch (UnsupportedEncodingException e) {
+                builder.append(presetName);
+            }
+        }
+        return builder.toString();
+    }
+
+    public static void resetUrl(){
+        //Log.d("MLUVII_URL", "RESET_URL");
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable(){
+
+            @Override
+            public void run(){
+                mluviiWebView.loadUrl(CHAT_URL);
+            }
+        });
+
+    }
+
+
+    /**
+     * Function that returns webview optimized for mluvii widget
+     * @param activity Activity for creating webview
+     * @param url url to load in webview
+     * @return WebView with loaded page with URL from param
+     */
+    public static WebView getMluviiWebView(final Activity activity, String url, String companyId, String tenantId, String presetName, String language){
+        if(mluviiWebView == null) {
+            CHAT_URL = createUrlString(url,companyId,tenantId,presetName,language);
+            Log.d("MLUVII_URL", CHAT_URL);
+            mluviiWebView = new WebView(activity);
+            /**
+             * Optimized WebviewClient without SSL ERROR to use it on localhost and not allowing to show url in native browser
+             */
+            mluviiWebView.setWebViewClient(new WebViewClient(){
+                                               @Override
+                                               public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                                                   /**
+                                                    * Otevre chatove URL v okne chatu
+                                                    */
+                                                   Log.d("MLUVII_URL_CHANGE","changing url to "+url);
+                                                   if(url.contains("GuestFrame?") || url.contains(CHAT_URL) ) {
+                                                       view.loadUrl(url);
+                                                       return false;
+                                                   } else {
+                                                       /**
+                                                        * Otevre jine URL (obrazek, file ...)
+                                                        */
+                                                       view.getContext().startActivity(
+                                                               new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                                                       return true;
+                                                   }
+                                               }
+
+                                               @Override
+                                               public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                                                   handler.proceed(); // Ignore SSL certificate errors
+                                               }
+
+                                               @Override
+                                               public void onPageFinished(WebView view, String url) {
+                                                   if(Build.VERSION.SDK_INT  >= 19) {
+                                                       mluviiWebView.evaluateJavascript(injectedString, null);
+                                                   } else{
+                                                       mluviiWebView.loadUrl("javascript: "+injectedString);
+                                                   }
+                                               }
+
+                                           }
+            );
+            /**
+             * Optimized WebChromeClient to Pass console messages and request permissions for camera, mic, etc. for future use
+             */
+            mluviiWebView.setWebChromeClient(new WebChromeClient(){
+
+                /*@Override
+                public void onPermissionRequest(final PermissionRequest request) {
+                    request.grant(request.getResources());
+                }*/
+
+                /*@Override
+                public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                    Log.d("MluviiLib", "CNSL_MSG: "+consoleMessage.message());
+                    return true;
+                }*/
+            });
+
+            mluviiWebView.getSettings().setJavaScriptEnabled(true);
+            mluviiWebView.getSettings().setDomStorageEnabled(true);
+            mluviiWebView.addJavascriptInterface(new InterfaceBetweenJavascriptAndLibrary(),"mluviiLibrary");
+
+            mluviiWebView.canGoBackOrForward(0);
+            mluviiWebView.loadUrl(CHAT_URL);
+        }
+        return mluviiWebView;
+    }
+
+}
---
+++ WebviewApp/app/src/main/java/mluviipoc/mluvii/com/webviewapp/MainActivity.java
@@ -0,0 +1,190 @@
+package mluviipoc.mluvii.com.webviewapp;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+import com.mluvii.mluviilibrary.MluviiLibrary;
+
+import java.util.concurrent.Callable;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MLUVII WEBVIEW APP";
+
+    private static WebView mluviiWebView = null;
+
+
+    /**
+     * Inicializacni parametry pro Mluvii library webView
+     */
+    private static String mluviiServer = "app.mluvii.com";
+    private static String mluviiCompanyId = "295b1064-cf5b-4a5d-9e05-e7a74f86ae5e";
+    private static String mluviiTenantId = null;
+    private static String mluviiPresetName = "DebitoTestSDK";
+    private static String mluviiLanguageCode = null;
+
+    /**
+     * Current stat of the operators group
+     */
+    private static int status = 0;
+    /**
+     * Customer button to indicate state of the operator and invole the webview
+     */
+    private Button btn = null;
+
+    /**
+     * CHAT_URL points on the chat widget. There can be more urls changed within the client app to direst
+     * chat to different operator groups/different widgets.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        /**
+         * Callback na Online stav z widgetu
+         */
+        MluviiLibrary.setStatusOnlineCallback(new Callable<Void>()  {
+            public Void call(){
+                Log.d("MLUVII_STATUS", "STATUS ONLINE");
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        btn.setBackgroundColor(Color.parseColor("#00ff00"));
+                        btn.setText("ONLINE");
+                    }
+                });
+                status = 1;
+                return null;
+            }
+        });
+
+        /**
+         * Callback na Busy stav z widgetu
+         */
+        MluviiLibrary.setStatusBusyCallback( new Callable<Void>()  {
+            public Void call(){
+                Log.d("MLUVII_STATUS", "STATUS BUSY");
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        btn.setBackgroundColor(Color.parseColor("#ffff00"));
+                        btn.setText("BUSY");
+                    }
+                });
+                status = 2;
+                return null;
+            }
+        });
+
+        /**
+         * Callback na Offline stav z widgetu
+         */
+        MluviiLibrary.setStatusOfflineCallback( new Callable<Void>()  {
+            public Void call(){
+                status = 0;
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        btn.setBackgroundColor(Color.parseColor("#ff0000"));
+                        btn.setText("OFFLINE");
+                    }
+                });
+                Log.d("MLUVII_STATUS", "STATUS OFFLINE");
+                return null;
+            }
+        });
+
+        MluviiLibrary.setChatLoadedCallback( new Callable<Void>()  {
+            public Void call(){
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        RelativeLayout.LayoutParams openedMluviiParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                        mluviiWebView.setLayoutParams(openedMluviiParams);
+                    }
+                });
+                return null;
+            }
+        });
+
+
+        /**
+         * Callback na Zavreni chatu - nutno reload puvodni URL neb chat widget to presmeruje
+         */
+        MluviiLibrary.setCloseChatFunc(new Callable<Void>() {
+            @Override
+            public Void call() throws Exception {
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        RelativeLayout.LayoutParams mluviiParams = new RelativeLayout.LayoutParams(0,0);
+                        //mluviiWebView.loadUrl(CHAT_URL);
+                        MluviiLibrary.resetUrl();
+                        mluviiWebView.setLayoutParams(mluviiParams);
+                    }
+                });
+                return null;
+            }
+        });
+
+        /**
+         * Definovani funkce pro kliknuti na odkaz
+         */
+
+
+
+        /**
+         * Inicializace WebView z MluviiLibrary
+         */
+        mluviiWebView = MluviiLibrary.getMluviiWebView(this, mluviiServer,mluviiCompanyId, mluviiTenantId, mluviiPresetName, mluviiLanguageCode);
+        /**
+         * Nastaveni velikosti 0,0 na webview, aby nebylo videt, dokud neni potreba
+         */
+        RelativeLayout.LayoutParams mluviiParams = new RelativeLayout.LayoutParams(0,0);
+        mluviiWebView.setLayoutParams(mluviiParams);
+
+
+        /**
+         * Tlaciko, ktere zobrazuje stav Widgetu - Seda, nespojeno se serverem - Cervena, offline - Zluta, busy - Zelena, online
+         */
+        btn = new Button(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(status == 1){
+                    MluviiLibrary.runChat();
+                    RelativeLayout.LayoutParams openedMluviiParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+                    mluviiWebView.setLayoutParams(openedMluviiParams);
+                }
+            }
+        });
+
+        /**
+         * Linear layout s Tlacitkem
+         */
+        LinearLayout linearLayout = new LinearLayout(this);
+        LinearLayout.LayoutParams params1 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+        linearLayout.setLayoutParams(params1);
+        linearLayout.addView(btn);
+
+        /**
+         * Nastaveni zakladniho layoutu
+         */
+        final RelativeLayout layout = new RelativeLayout(this);
+        final RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+        layout.addView(linearLayout);
+        layout.addView(mluviiWebView);
+        layout.setLayoutParams(params);
+        setContentView(layout);
+    }
+}
+
